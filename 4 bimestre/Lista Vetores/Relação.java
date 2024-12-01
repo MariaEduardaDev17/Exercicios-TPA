1c05f608acf026e7d6d53983f7522b15b40cc266
MariaEduardaDev17 <marydaygisa08@gmail.com>
2024-12-01 17:00:47 -0300

    Update Relação.java

--- 4 bimestre/Lista Vetores/Relação.java
+++ 4 bimestre/Lista Vetores/Relação.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Relação {
+public class RelaÃ§Ã£o {
 	public static void main(String args[]) {
 		Scanner ler = new Scanner (System.in);
 		final int TAM=10;
@@ -7,14 +7,13 @@ public class Relação {
 		int a[] = new int [TAM];
 		
 		for(i=0; i<TAM; i++) {
-			System.out.println("Digite o "+(i+1)+" posição do vetor a: ");
+			System.out.println("Digite o "+(i+1)+" posiÃ§Ã£o do vetor a: ");
 			a[i]= ler.nextInt();
 		}
 		
 		for(i=0; i<TAM; i++) {
 			System.out.println("Elemento a[" + i + "]=" + a[i] + "\n");
-			System.out.println("Pares de 0 até " + a[i] + ": \n");
-			
+			System.out.println("Pares de 0 atÃ© " + a[i] + ": \n");
 			
 			for(j=0; j<=a[i]; j+=2) {
 				System.out.print(j + " ");
